070e5ee846aa108831b1c621cb6649318fda84a1
Bryan Flores <bryanfloresmolina08@gmail.com>
2025-10-22 09:00:51 -0400

    theme and colors of the app done

--- app/src/main/java/com/example/urbane/ui/theme/Theme.kt
+++ app/src/main/java/com/example/urbane/ui/theme/Theme.kt
@@ -12,31 +12,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Green,
+    background = DarkGray,
+    surface = DarkGray,
+    onBackground = White,
+    onSurface = White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Green,
+    background = White,
+    surface = White,
+    onBackground = DarkGray,
+    onSurface = DarkGray
 )
 
 @Composable
 fun UrbaneTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
